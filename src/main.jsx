cccd3947b59604956d75af7bd428cb3e0ae961c4
Md. Zahidur Rahman <45038726+zahidtdx61@users.noreply.github.com>
2024-05-03 11:19:31 +0600

    added redux toolkit

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
+import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
 import "./index.css";
 import router from "./routes/Router.jsx";
+import { store } from "./store/store.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <HelmetProvider>
-      <RouterProvider router={router} />
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
     </HelmetProvider>
   </React.StrictMode>
 );
