6c36f5a1d79b55f31afc85cf49a44bca8ced451d
Md. Zahidur Rahman <45038726+zahidtdx61@users.noreply.github.com>
2024-05-03 12:03:03 +0600

    added reducer for user data

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,5 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import UserReducer from "./reducers/UserReducer";
+
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    UserReducer: UserReducer,
+  },
 });
